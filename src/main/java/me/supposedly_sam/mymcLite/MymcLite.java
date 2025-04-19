9d105d9ff7b6618eb3741c9620e10eb5ea7640f2
Mevin-JR <supposedlysamyt@gmail.com>
2025-04-19 16:14:09 +0530

    Dynamic game selector

--- src/main/java/me/supposedly_sam/mymcLite/MymcLite.java
+++ src/main/java/me/supposedly_sam/mymcLite/MymcLite.java
@@ -1,13 +1,26 @@
 package me.supposedly_sam.mymcLite;
 
-import me.supposedly_sam.mymcLite.Commands.Build;
-import me.supposedly_sam.mymcLite.Commands.SetSpawn;
+import com.google.common.collect.Iterables;
+import me.supposedly_sam.mymcLite.Commands.*;
+import me.supposedly_sam.mymcLite.Events.BuildEvents;
+import me.supposedly_sam.mymcLite.Events.Chat;
+import me.supposedly_sam.mymcLite.Events.DisabledCommands;
+import me.supposedly_sam.mymcLite.Events.JoinEvent;
+import me.supposedly_sam.mymcLite.Utils.PluginMessages;
+import me.supposedly_sam.mymcLite.Utils.SpawnFile;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public final class MymcLite extends JavaPlugin {
 
@@ -16,11 +29,27 @@ public final class MymcLite extends JavaPlugin {
 
         // Config.yml
         saveDefaultConfig();
+        // Spawn.yml
+        new SpawnFile(this);
+
+        // Channel Registers
+        PluginMessages pluginMessages = new PluginMessages();
+        Bukkit.getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+        Bukkit.getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", pluginMessages);
+
+        // Event Registers
+        registerListener(new JoinEvent(this));
+        registerListener(new BuildEvents(this));
+        registerListener(new Chat(this));
+        registerListener(new DisabledCommands(this));
 
         // Command Registers
+        registerCommand("mlreload", new Reload(this));
         registerCommand("setspawn", new SetSpawn(this));
+        registerCommand("spawn", new Spawn(this));
         registerCommand("build", new Build());
-
+        registerCommand("goto", new Goto(this));
+        registerCommand("gameselector", new GameSelector(this));
 
     }
 
--- src/main/java/me/supposedly_sam/mymcLite/Utils/Responses.java
+++ src/main/java/me/supposedly_sam/mymcLite/Utils/Responses.java
@@ -8,12 +8,22 @@ import org.bukkit.entity.Player;
 public class Responses {
 
     public static void sendNotAuthorized(CommandSender target) {
-        String notAuthorizedMessage = ChatColor.translateAlternateColorCodes('&', "&c✘ You are not authorized to perform that.");
+        String notAuthorizedMessage = ChatColor.translateAlternateColorCodes('&', "&c[✘] You are not authorized to perform that.");
         target.sendMessage(notAuthorizedMessage);
     }
 
     public static void sendCommandUsage(CommandSender target, String command) {
-        String commandUsageMessage = ChatColor.translateAlternateColorCodes('&', "&c✘ Usage: " + command);
+        String commandUsageMessage = ChatColor.translateAlternateColorCodes('&', "&c[✘] Usage: " + command);
         target.sendMessage(commandUsageMessage);
+    }
+
+    public static void sendNonPlayerExecution(CommandSender target) {
+        String nonPlayerExecutionMessage = ChatColor.translateAlternateColorCodes('&', "&c[✘] This command can only be executed by a player.");
+        target.sendMessage(nonPlayerExecutionMessage);
+    }
+
+    public static void sendDisabledCommand(CommandSender target){
+        String disabledCommandMessage = ChatColor.translateAlternateColorCodes('&', "&c[✘] This command has been disabled.");
+        target.sendMessage(disabledCommandMessage);
     }
 }
